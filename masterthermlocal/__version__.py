6d3f89f1edce6a9df8e88b1250ebdd6cc2310bd9
Richard Holmes <richard@shedc.uk>
2025-05-27 20:53:49 +0000

    Initial Setup

---
+++ masterthermlocal/__version__.py
@@ -0,0 +1,3 @@
+"""Python API wrapper for Mastertherm Connect."""
+
+__version__ = "0.0.0"
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+"""Test the Main command."""
+
+
+def test_help(capsys):
+    """Test the Main Help."""
+    pass
